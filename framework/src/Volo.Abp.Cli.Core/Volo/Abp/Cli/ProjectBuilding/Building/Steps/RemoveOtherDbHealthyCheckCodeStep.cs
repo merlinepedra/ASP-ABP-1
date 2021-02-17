7ae40b8681ff8b11f131bdb97da759dc2719fe8b
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2021-02-17 13:33:25 +0300

    Create RemoveOtherDbHealthyCheckCodeStep.cs

---
+++ framework/src/Volo.Abp.Cli.Core/Volo/Abp/Cli/ProjectBuilding/Building/Steps/RemoveOtherDbHealthyCheckCodeStep.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Volo.Abp.Cli.ProjectBuilding.Files;
+
+namespace Volo.Abp.Cli.ProjectBuilding.Building.Steps
+{
+    public class RemoveOtherDbHealthyCheckCodeStep : ProjectBuildPipelineStep
+    {
+        public override void Execute(ProjectBuildContext context)
+        {
+        }
+    }
+}
